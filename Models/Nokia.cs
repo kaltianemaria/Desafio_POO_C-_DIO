f5f8a1a3e41d1253475aa81b6598672e9d8d2447
Kaltiane Maria <kalfisio@hotmail.com>
2024-01-07 15:04:30 -0300

    finish project

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,19 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    public class Nokia : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        public Nokia(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+        }
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando o aplicativo {nomeApp} no Nokia");
+        }
+
+        public override string ToString()
+        {
+            return $"Nokia - Modelo: {this.Modelo}, Número: {this.Numero}, IMEI: {this.Imei}, Memória: {this.Memoria}GB";
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Smartphone nokia = new Nokia(numero: "987654321", modelo: "G60", imei: "123456789", memoria: 64);
+Console.WriteLine($"Nokia: {nokia.ToString()}");
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Netflix");
+
+Console.WriteLine("\n");
+
+Smartphone iphone = new Iphone(numero: "123456789", modelo: "Iphone 11", imei: "987654321", memoria: 256);
+Console.WriteLine($"Iphone: {iphone.ToString()}");
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
