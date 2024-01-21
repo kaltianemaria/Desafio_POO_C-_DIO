2fa34492fe5e9c176dd34cbcedf6734e0189be62
Kaltiane Maria <kalfisio@hotmail.com>
2024-01-21 15:29:40 -0300

    add protected to attributes abstract class

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,10 +3,10 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
 
-        public string Numero { get; set; }
-        public string Modelo { get; set; }
-        public string Imei { get; set; }
-        public int Memoria { get; set; }
+        protected string Numero;
+        protected string Modelo;
+        protected string Imei;
+        protected int Memoria;
 
 
         public Smartphone(string numero, string modelo, string imei, int memoria)
@@ -14,6 +14,45 @@ namespace DesafioPOO.Models
             Numero = numero;
             Modelo = modelo;
             Imei = imei;
+            Memoria = memoria;
+        }
+
+        public string GetNumero()
+        {
+            return Numero;
+        }
+
+        public void setNumero(string numero)
+        {
+            Numero = numero;
+        }
+
+        public string GetModelo()
+        {
+            return Modelo;
+        }
+
+        public void SetModelo(string modelo)
+        {
+            Modelo = modelo;
+        }
+        public string GetImei()
+        {
+            return Imei;
+        }
+
+        public void SetImei(string imei)
+        {
+            Imei = imei;
+        }
+
+        public int GetMemoria()
+        {
+            return Memoria;
+        }
+
+        public void SetMemoria(int memoria)
+        {
             Memoria = memoria;
         }
 
--- Program.cs
+++ Program.cs
@@ -9,7 +9,10 @@ nokia.InstalarAplicativo("Netflix");
 Console.WriteLine("\n");
 
 Smartphone iphone = new Iphone(numero: "123456789", modelo: "Iphone 11", imei: "987654321", memoria: 256);
-Console.WriteLine($"Iphone: {iphone.ToString()}");
+Console.WriteLine($"Iphone: {iphone}");
+iphone.SetModelo("Iphone XR");
+Console.WriteLine($"Iphone: {iphone}");
+Console.WriteLine($"Iphone imei: {iphone.GetImei()}");
 iphone.Ligar();
 iphone.ReceberLigacao();
 iphone.InstalarAplicativo("Instagram");
